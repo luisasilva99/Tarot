d7bf68a00135f30826ce3aeb23ca8d6a5d7ba402
luisasilva99 <luisasilvamartinez1999@gmail.com>
2025-08-06 13:11:23 +0200

    read cards with get

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,37 @@
+import { useEffect, useState } from "react";
+import { getAllCards } from "../services/TarotServices";
+import './Home.css'
+
 const Home = () => {
+    const [cards, setCards] = useState([]);
+
+    useEffect(() => {
+        const fetchCards = async () => {
+            try {
+            const data = await getAllCards();
+            console.log(data);
+            setCards(data);
+            } catch (error) {
+                console.error("Error fetching cards:", error);
+            }
+        };
+
+        fetchCards();
+    }, []);
+
   return (
-    <div>
-      <h1>Página de inicio - Tarot STEM</h1>
+    <>
+    <h1>Página de inicio - Tarot STEM</h1>
+    <div className="card-grid">
+        {cards.map((card) => (
+        <div key={card.id} className="card">
+            <img src={card.arcaneImage.imageSrc} alt={card.arcaneName} className="card-img"/>
+            <h3>{card.arcaneName}</h3>
+            <p>{card.arcaneDescription}</p>
+        </div>
+      ))}
     </div>
+    </>
   );
 };
 
--- src/services/TarotServices.jsx
+++ src/services/TarotServices.jsx
@@ -1,5 +1,16 @@
-const TAROT_API_URL = 'https://6872278c76a5723aacd3cbb3.mockapi.io/api/v1/tarot';
-//MÉTODO GET PARA EL READ getAllTarotCards
-async function getAllTarotCards() {
+import axios from 'axios';
 
-}
+const TAROT_API_URL = 'https://6872278c76a5723aacd3cbb3.mockapi.io/api/v1/tarot';
+
+export const getAllCards = async () => {
+    try {
+        const response = await axios.get(TAROT_API_URL);
+        return response.data;
+    } catch (error) {
+        console.error("Error al obtener cartas", error);
+        return[];
+    }
+};
+
+
+
